eaac18ef78f4cb165d55bc500e1e887bd6ca2246
Matthew_Dreemurr <hadadmat@gmail.com>
2021-01-18 22:38:10 +0100

    try fix

--- norminette_install.sh
+++ norminette_install.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
+# exit when any command fails
+set -e
+
+# keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+# echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 clear
 printf "\033[32mUpdating all packages\n\033[m"
-if sudo apt-get update && sudo apt-get upgrade && clear
-else exit 1
+sudo apt-get update && sudo apt-get upgrade && clear
 printf "\033[32mInstall all required packages\n\033[m"
-if sudo apt-get install zsh git ruby ruby-bundler ruby-dev build-essential && clear
-else exit 1
+sudo apt-get install zsh git ruby ruby-bundler ruby-dev build-essential && clear
 printf "\033[32mInstall Norminette\n\033[m"
-if git clone https://github.com/42Paris/norminette.git ~/.norminette/ && \
+git clone https://github.com/42Paris/norminette.git ~/.norminette/ && \
 cd ~/.norminette/ && \
 sudo bundle && clear
-else exit 1
 printf "\033[32mAdd zsh alias\n\033[m"
-if echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.zshrc && source ~/.zshrc
-else exit 1
+echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.zshrc && source ~/.zshrc
 printf "\033[32mAdd bash alias\n\033[m"
-if echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.bashrc && source ~/.bashrc
-else exit 1
+echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.bashrc && source ~/.bashrc
