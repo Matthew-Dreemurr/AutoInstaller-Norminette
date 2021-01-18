7b2f425ddb5568af1056ac0cb7d176d4635bef36
Matthew_Dreemurr <hadadmat@gmail.com>
2021-01-18 22:35:38 +0100

    try fix stop @ error

--- norminette_install.sh
+++ norminette_install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-set -e
 clear
 printf "\033[32mUpdating all packages\n\033[m"
-sudo apt-get update && sudo apt-get upgrade && clear
+if sudo apt-get update && sudo apt-get upgrade && clear
+else exit 1
 printf "\033[32mInstall all required packages\n\033[m"
-sudo apt-get install zsh git ruby ruby-bundler ruby-dev build-essential && clear
+if sudo apt-get install zsh git ruby ruby-bundler ruby-dev build-essential && clear
+else exit 1
 printf "\033[32mInstall Norminette\n\033[m"
-git clone https://github.com/42Paris/norminette.git ~/.norminette/ && \
+if git clone https://github.com/42Paris/norminette.git ~/.norminette/ && \
 cd ~/.norminette/ && \
 sudo bundle && clear
+else exit 1
 printf "\033[32mAdd zsh alias\n\033[m"
-echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.zshrc && source ~/.zshrc
+if echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.zshrc && source ~/.zshrc
+else exit 1
 printf "\033[32mAdd bash alias\n\033[m"
-echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.bashrc && source ~/.bashrc
+if echo 'alias norminette="~/.norminette/norminette.rb"' >> ~/.bashrc && source ~/.bashrc
+else exit 1
